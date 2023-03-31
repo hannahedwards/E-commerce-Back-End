9663e20b2251d8ff1fb0e8c7a3ba04163b6bd1e8
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-31 00:27:14 -0400

    model index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  unique: false
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  unique: false
+});
 module.exports = {
   Product,
   Category,
